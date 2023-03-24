c1c224e0882cbce68453a08eb576ae0f27ff7b4a
Gavkhar Saryeva <eibova94@mail.ru>
2023-03-24 10:53:38 +0300

    autotest edit

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -5,7 +5,9 @@ import org.junit.Before
 import org.junit.Test
 
 class WallServiceTest {
-
+    val post1 = Post(1, 2, 3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = null, views = View(0))
+    val post2 = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post", views = View(10))
+    val comment = Comment(1, 2)
     @Before
     fun clearBeforeTest() {
         WallService.clear()
@@ -13,8 +15,6 @@ class WallServiceTest {
 
     @Test
     fun updateExisting() {
-        val post1 = Post(1, 2, 3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = null, views = View(0))
-        val post2 = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post", views = View(10))
         WallService.add(post1)
         WallService.add(post2)
         val update = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post edit", views = View(15))
@@ -24,8 +24,6 @@ class WallServiceTest {
 
     @Test
     fun updateNotExisting() {
-        val post1 = Post(1, 2, 3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = null, views = View(0))
-        val post2 = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post", views = View(10))
         WallService.add(post1)
         WallService.add(post2)
         val update = Post(49,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post edit", views = View(15))
@@ -35,55 +33,52 @@ class WallServiceTest {
 
     @Test
     fun addNotZero() {
-        val post = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post edit", views = View(15))
-        WallService.add(post)
-        val result = post.id
+        WallService.add(post2)
+        val result = post2.id
         assertEquals(4, result)
     }
 
     @Test
     fun createCommentId() {
-        val post = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post edit", views = View(15))
-        WallService.add(post)
-        WallService.createComment(4, comment = Comment(1, 2))
+        WallService.add(post2)
+        WallService.createComment(4, comment)
     }
 
     @Test(expected = PostNotFoundException::class)
     fun createCommentNotId() {
-        val post = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post edit", views = View(15))
-        WallService.add(post)
-        WallService.createComment(5, comment = Comment(3,4))
+        WallService.add(post2)
+        WallService.createComment(5, comment)
     }
 
     @Test
     fun negativeCommentNotificationAdd() {
-        var post2 = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post", views = View(10))
         WallService.add(post2)
-        WallService.createComment(4, comment = Comment(1, 2))
-        val reportComment = ReportComment(2, 3, 0)
-    } // не работает
+        WallService.createComment(4, comment)
+        val reportComment = ReportComment(1, 2, 0)
+        WallService.negativeCommentNotification(reportComment)
+    }
 
     @Test(expected = NotCommentIdException::class)
     fun negativeCommentNotificationNotCommentId() {
-        var post2 = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post", views = View(10))
         WallService.add(post2)
-        WallService.createComment(4, comment = Comment(1, 2))
+        WallService.createComment(4, comment)
         val reportComment = ReportComment(2, 2, 0)
-    } // не работает
+        WallService.negativeCommentNotification(reportComment)
+    }
 
     @Test(expected = NotOwnerIdException::class)
     fun negativeCommentNotificationNotOwnerId() {
-        var post2 = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post", views = View(10))
         WallService.add(post2)
-        WallService.createComment(4, comment = Comment(1, 2))
+        WallService.createComment(4, comment)
         val reportComment = ReportComment(1, 3, 0)
-    } // не работает
+        WallService.negativeCommentNotification(reportComment)
+    }
 
     @Test(expected = NotReasonException::class)
     fun negativeCommentNotificationNotReason() {
-        var post2 = Post(4,7,3, replyOwnerId = 5, replyPostId = 6, authorName = "Petya", copyRight = "post", views = View(10))
         WallService.add(post2)
-        WallService.createComment(4, comment = Comment(1, 2))
-        val reportComment = ReportComment(1, 3, 9)
-    } // не работает
+        WallService.createComment(4, comment)
+        val reportComment = ReportComment(1, 2, 9)
+        WallService.negativeCommentNotification(reportComment)
+    }
 }
